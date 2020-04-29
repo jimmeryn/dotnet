565a94b467a7ce0f23f3806aa90bbc2fd58f4c62
jimmeryn <jimmeryn@gmail.com>
2020-04-29 17:43:07 +0200

    Storing data of current logged user in app

--- App.xaml.cs
+++ App.xaml.cs
@@ -27,7 +27,26 @@ namespace WorkHoursTracker
       }
     }
 
+    public int EmployeeId { get; set; }
     public string Name { get; set; }
     public string Surname { get; set; }
+    public string JobTitle { get; set; }
+    public Nullable<int> CurrentTimeId { get; set; }
+
+    public void CreateCurrentEmployee(string name, string surname)
+    {
+      Name = name;
+      Surname = surname;
+      using (var db = new DataBaseContext())
+      {
+        var currentEmployee = db.Employees.FirstOrDefault(user => user.Name == name && user.Surname == surname);
+        if (currentEmployee != null)
+        {
+          EmployeeId = currentEmployee.EmployeeId;
+          JobTitle = currentEmployee.JobTitle;
+          CurrentTimeId = currentEmployee.CurrentTimeId;
+        }
+      }
+    }
   }
 }
