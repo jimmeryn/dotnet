fc972db0c5575391ddb266c3c4a8f605c423afdf
jimmeryn <jimmeryn@gmail.com>
2020-04-29 17:46:08 +0200

    Working state dependency

--- src/Views/RegisterTimeView.xaml.cs
+++ src/Views/RegisterTimeView.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 
+using WorkHoursTracker.Models;
+
 namespace WorkHoursTracker.Views
 {
   /// <summary>
@@ -25,6 +27,7 @@ namespace WorkHoursTracker.Views
     {
       InitializeComponent();
       StartClock();
+      UpdateWorkState();
     }
 
     private void StartClock()
@@ -38,6 +41,65 @@ namespace WorkHoursTracker.Views
     private void tickevent(object sender, EventArgs e)
     {
       datalbl.Text = DateTime.Now.ToString();
+    }
+
+    private void UpdateWorkState()
+    {
+      if (((App)Application.Current).CurrentTimeId != null)
+      {
+        EnableWorkingButtons(true);
+        State.Text = "At Work";
+      }
+      else
+      {
+        EnableWorkingButtons(false);
+        State.Text = "Not Working";
+      }
+    }
+
+    private void EnableWorkingButtons(bool isWorking)
+    {
+      StartWorkingButton.IsEnabled = !isWorking;
+      StopWorkingButton.IsEnabled = isWorking;
+    }
+
+    public void registerTime(object sender, RoutedEventArgs e)
+    {
+      // check if curren employee is working or not and set start or stop time acordingly
+      using (var context = new DataBaseContext())
+      {
+        try
+        {
+          context.Database.EnsureCreated();
+          var currentEmployee = context.Employees.FirstOrDefault(user =>
+              user.Name == ((App)Application.Current).Name &&
+              user.Surname == ((App)Application.Current).Surname);
+          if (currentEmployee != null)
+          {
+            var time = new Time
+            {
+              StartDate = DateTime.Now,
+              EndDate = DateTime.Now,
+              EmployeeId = currentEmployee.EmployeeId
+            };
+            context.Times.Add(time);
+            currentEmployee.Times.Add(time);
+            context.Employees.Update(currentEmployee);
+            foreach (var item in context.Employees.FirstOrDefault().Times)
+              System.Console.WriteLine(item.TimeId);
+            context.SaveChanges();
+          }
+        }
+        catch (Microsoft.EntityFrameworkCore.DbUpdateException ex)
+        {
+          Console.WriteLine(ex.Entries);
+          throw ex;
+        }
+        catch (System.Exception ex)
+        {
+          throw ex;
+        }
+      }
     }
   }
 }
